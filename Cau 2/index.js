b891890bf2e4d787ec5f89457fb307759de8d3e2
nguyenminh189 <121212123+nguyenminh189@users.noreply.github.com>
2023-06-20 23:14:55 +0700

    Add files via upload

---
+++ Cau 2/index.js
@@ -0,0 +1,14 @@
+let A = prompt("Enter your number: ") ;
+let Num = Number(A);
+
+function numberOneTriangle(number) {
+    for (let C=0; C<=number; C++) {
+        let rows = '';
+         for (let B=0; B<=C; B++) {
+            rows += '* ';
+             }
+        console.log(row);
+    }
+}
+
+numberOneTriangle(Num);
